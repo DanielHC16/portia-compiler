f20ee155e4e86bc096c14e4ddeb56229e3dfb22a
nova-vltn <137620732+nova-vltn@users.noreply.github.com>
2026-03-02 19:22:14 +0800

    *feat: add required delimiters for lexer tokenization

--- lexer-backend/app/lexer/delimiters.py
+++ lexer-backend/app/lexer/delimiters.py
@@ -21,12 +21,12 @@ class Delimiters:
         # Patterns: (int)expr -- cast syntax, int func(int arr[5]) -- array params
         # Also allows ')' for cast patterns like (int)x where dtype follows '('
         # Excludes 'void' and 'weave' (not valid cast targets)
-        self.dtype_delim = chars.whitespace + ['(', '[', ')'] + chars.newline
+        self.dtype_delim = chars.whitespace + [')'] + chars.newline
 
         # RESERVED SYMBOLS DELIMITER
         # Arithmetic and logical operator delimiters
         self.negative_delim = chars.alphanum + chars.whitespace + ['(']
-        self.marithmetic_delim = chars.alphanum + chars.whitespace + ['(']
+        self.marithmetic_delim = chars.alphanum + chars.whitespace + ['(', '-']
         self.logical_op_delim = chars.alphanum + chars.whitespace + ['(', '-']
         self.exclamation_delim = chars.alphabetics + ['(', '!']
         self.unary_delim = chars.alphabetics + chars.whitespace + [';', ')', ']', ',', '}'] + chars.newline
@@ -34,9 +34,9 @@ class Delimiters:
         
         # Grouping symbol delimiters
         self.open_paren_delim = chars.alphanum + chars.whitespace + ['"', '!', ')', '-', '(', ';', "'", '..'] 
-        self.close_paren_delim = chars.alphanum + ['+', '-', '*', '/', '%', '>', '<','!', '=', '&', '|', '{', ';', ')', '(',  "'",',','..'] + chars.whitespace + chars.newline
+        self.close_paren_delim = chars.alphanum + ['+', '-', '*', '/', '%', '>', '<','!', '=', '&', '|', '{', ';', ')', '(',  "'",',','..', '"' ] + chars.whitespace + chars.newline
         self.close_bracket_delim = ['+', '-', '*', '/', '%', '=', ')', ';', ',', '[',  '>', '<', '!', '=', '&', '|', '..'] + chars.whitespace
-        self.open_curly_delim = chars.whitespace + chars.newline + chars.alphanum + ['{', '"', "'", '-', '!']
+        self.open_curly_delim = chars.whitespace + chars.newline + chars.alphanum + ['{', '"', "'", '-', '!','}']
         self.close_curly_delim = chars.whitespace + chars.newline + [';', ',', None] + chars.alphabetics + ['}']
 
         # Punctuation delimiters
@@ -66,7 +66,7 @@ class Delimiters:
         self.char_lit_delim = chars.whitespace + chars.newline + [')', ';', '}', ',', ':', '..']
         
         # Boolean literal delimiters (true, false)
-        self.bool_lit_delim = ['=', '!', '&', '|', ',', ')',':', ';'] + chars.whitespace + chars.newline
+        self.bool_lit_delim = ['=', '!', '&', '|', ',', ')',':', ';', '}'] + chars.whitespace + chars.newline
         
         # Numerical literal delimiters (int, long, float, double)
         self.nbl_delim = ['+', '-', '*', '/', '%', '>', '<', '=', '!', ',', ')', ']', '}', ';', ':', '&', '|'] + chars.whitespace + chars.newline + ['..']
