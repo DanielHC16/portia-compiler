1bf8e5fa93cd78c395ee1e6e5bb9e301c9aacd6a
Daniel Camacho <danielcamacho0416@gmail.com>
2026-05-05 20:38:27 +0800

    chore: updated str/char_lit_delims

--- lexer-backend/app/lexer/delimiters.py
+++ lexer-backend/app/lexer/delimiters.py
@@ -57,10 +57,10 @@ class Delimiters:
 
         # LITERALS DELIMITER
         # String literal delimiters
-        self.str_lit_delim = chars.whitespace + chars.newline + [')', ';', '}', ',', ':', '..']
+        self.str_lit_delim = chars.whitespace + chars.newline + [')', ';', '}', ',', ':', '..', '&', '|']
         
         # Character literal delimiters
-        self.char_lit_delim = chars.whitespace + chars.newline + [')', ';', '}', ',', ':', '..','>', '<', '=', '!']
+        self.char_lit_delim = chars.whitespace + chars.newline + [')', ';', '}', ',', ':', '..','>', '<', '=', '!', '&', '|']
         
         # Boolean literal delimiters (true, false)
         self.bool_lit_delim = ['=', '!', '&', '|', ',', ')',':', ';', '}'] + chars.whitespace + chars.newline
